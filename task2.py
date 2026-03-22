4b7e734cb406454c5cc198eaecc32773d813ef45
nyutxxrws <nyutxxrws@gmail.com>
2026-03-22 16:41:33 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,13 @@
+import random
+import math
+import statistics
+numbers = [random.randint(1, 100) for _ in range(100)]
+mean_value = statistics.mean(numbers)
+median_value = statistics.median(numbers)
+stdev_value = statistics.stdev(numbers)
+sum_numbers = sum(numbers)
+sqrt_sum = round(math.sqrt(sum_numbers), 2)
+print(f"Среднее: {mean_value:.2f}, "
+      f"Медиана: {median_value}, "
+      f"Стандартное отклонение: {stdev_value:.2f}, "
+      f"Корень из суммы: {sqrt_sum}")
